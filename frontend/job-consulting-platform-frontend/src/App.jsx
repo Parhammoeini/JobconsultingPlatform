f35361c43f767e3e9c08561e21ad41c2a03ea4fe
Jonathan quijano <123256194+CursedHen@users.noreply.github.com>
2026-03-31 14:24:07 -0400

    Update App.jsx
    added PolicyManager, and ConsultantApproval

--- frontend/job-consulting-platform-frontend/src/App.jsx
+++ frontend/job-consulting-platform-frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import Availability from "./Availability";
 import Booking from "./Booking";
 import SystemStatus from "./SystemStatus";
+import ConsultantApproval from "./ConsultantApproval";
+import PolicyManager from "./PolicyManager";
 
 function App() {
   return (
     <div>
       <h1>Consultant Dashboard</h1>
+
+      <SystemStatus />
+
+      <hr />
+
+      <ConsultantApproval />
+
+      <hr />
+
+      <PolicyManager />
+
+      <hr />
 
       <Availability />
 
